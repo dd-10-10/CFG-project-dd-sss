d343ba91d9d9e534ecf9fc1539bd2c42bd6d99fd
dd-10-10 <dheeraj.deshpande@students.iiserpune.ac.in>
2026-03-19 07:13:11 +0000

    Added comprehension of ATAC state and some very basic docstrings

--- SVMScripts/new_script.py
+++ SVMScripts/new_script.py
@@ -6,6 +6,9 @@ from globals import seq_to_pos
 
 
 def seq_kmer_count(k, seq):
+    '''
+    Function to count k-mers in seq
+    '''
     l = len(seq)
     counts = np.zeros(4**k)
     for i in range(l-k):
@@ -13,24 +16,35 @@ def seq_kmer_count(k, seq):
     return counts
 
 def seq_vec(file, k):
+    '''
+    Function to make vectors from k-mers for sequences in file
+    '''
+    atac= []
     ids= []
     vecs= []
     for record in SeqIO.parse(file, "fasta"):
         kmer= seq_kmer_count(k, str(record.seq))
         vecs.append(kmer)
         ids.append(record.id[-1])
+        atac.append(record.id[-4])
     vecs= np.array(vecs)
-    return ids, vecs
+    return atac, ids, vecs
 
 def kmer_svm(file, k):
-    ids, vecs= seq_vec(file, k)
+    '''
+    Function to perform k-mer svm with data in file
+    '''
+    atac, ids, vecs= seq_vec(file, k)
     mmc= svm.SVC(kernel= "linear")
     mmc.fit(vecs, ids)
     return mmc
 
 def test(file, t_file, k):
+    '''
+    Test function
+    '''
     clfr= kmer_svm(file, k)
-    t_ids, t_vecs= seq_vec(t_file, k)
+    t_atac, t_ids, t_vecs= seq_vec(t_file, k)
     t_pred= clfr.predict(t_vecs)
     match= t_pred== t_ids
     acc= match.sum()/len(match)
